58823312aa40a9e3c47fe73e2c0d8893dbffe5b4
backdfb <>
2024-06-03 10:26:28 +0900

    레시피아이템 컴퍼넌트 인덱스에서 삭제

--- src/pages/MainPage/page/index.tsx
+++ src/pages/MainPage/page/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ReciepeItem from "@components/reciepItem/ReciepeItem";
 
 const MainPage = () => {
   return (
     <div className="grid gap-2">
       <p>MainPage</p>
-      <ReciepeItem />
     </div>
   );
 };
