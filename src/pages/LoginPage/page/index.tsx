1ea59585d30703c2464d457025064c51d09c2e7d
backdfb <>
2024-06-03 22:14:20 +0900

    style: loginpage ui 수정/Modal 생성

--- src/pages/LoginPage/page/index.tsx
+++ src/pages/LoginPage/page/index.tsx
@@ -1,18 +1,34 @@
-import React from "react";
-import bi from "@assets/bi.png";
-import GoogleLoginButton from "./GoogleLoginButton";
-import KakaoLoginButton from "./KakaoLoginButton";
+import React, { useState } from "react";
+import bi from "@assets/icons/bi.png";
+import GoogleLoginButton from "../components/GoogleLoginButton";
+import KakaoLoginButton from "../components/KakaoLoginButton";
+import Modal from "./Modal";
 
 const LoginPage: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSubmit = (name: string, gender: string, age: string) => {
+    console.log("Submitted Info:", { name, gender, age });
+  };
+
   return (
-    <div className="flex flex-col items-center p-8 pt-[100px]">
+    <div className="flex flex-col items-center pt-[100px]">
       <div className="w-[350px] h-[350px]">
         <img src={bi} alt="BrandIcon" className="mx-auto"></img>
       </div>
-      <div className="flex flex-col gap-5 mt-[75px]">
-        <KakaoLoginButton />
+      <div className="flex flex-col gap-4 mt-[75px]">
+        <KakaoLoginButton onClick={handleLoginClick} />
         <GoogleLoginButton />
       </div>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} onSubmit={handleSubmit} />
     </div>
   );
 };
