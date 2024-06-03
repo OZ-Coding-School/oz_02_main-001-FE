879f127815191f9234b7e854d8a9d8900e60593a
jjaeho0415 <jjh0773@naver.com>
2024-06-03 17:09:42 +0900

    Chore: SettingPage 추가 및 라우팅 설정

---
+++ src/pages/SettingPage/page/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@components/header/Header";
+import React from "react";
+
+const SettingPage: React.FC = () => {
+  return (
+    <>
+      <Header hasBackBtn={true} title="설정" />
+      <div>SettingPage</div>
+    </>
+  );
+};
+
+export default SettingPage;
