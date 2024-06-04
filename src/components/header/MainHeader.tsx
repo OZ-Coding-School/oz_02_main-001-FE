be4c8956535b1d15752df713d939ed5107395e3c
jjaeho0415 <jjh0773@naver.com>
2024-06-04 18:03:49 +0900

    Refactor: 메인페이지에서 쓰이는 헤더 컴포넌트 수정

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -13,9 +13,9 @@ const MainHeader: React.FC = () => {
     navigate("/search");
   };
   return (
-    <div className="flex justify-center items-center h-[50px] relative border-b-[1px]">
+    <div className="flex justify-center items-center h-[50px] relative ">
       <div className="flex justify-end w-[450px] cursor-pointer" onClick={handleClick}>
-        <div className="flex items-center w-[98%] h-[38px] pl-2 rounded-3xl bg-iceBlue text-[#64748B] ">
+        <div className="flex items-center w-[98%] h-[35px] pl-2 rounded-3xl bg-iceBlue text-[#64748B] ">
           <IoIosSearch className="w-[24px] h-[24px] mr-2" />
           <span>Search</span>
         </div>
