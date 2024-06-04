2debe6cec9c5d7898bc11b09749486a4bf26ea3b
jjaeho0415 <jjh0773@naver.com>
2024-06-04 18:09:16 +0900

    Style: 프로필페이지 레이아웃 작업 완료(레시피 페이지의 댓글부분과 연결만 하면됨)

--- src/pages/ProfilePage/page/index.tsx
+++ src/pages/ProfilePage/page/index.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Header from "@components/header/Header";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PostsList from "../../../pages/MyPage/components/PostsList";
+import ProfileSection from "../../../pages/MyPage/components/ProfileSection";
+import { accountData } from "../../../pages/MyPage/page";
 
-const ProfilePage = () => {
-  return <div></div>;
+// interface ProfilePageProps {
+//   userId?: number;
+// }
+
+const ProfilePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [userNickname, setUserNickname] = useState<string>("");
+
+  useEffect(() => {
+    // api get으로 데이터 받아올 예정
+    setUserNickname(accountData.nickname);
+  }, []);
+
+  const handleBackBtnClick = (): void => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <Header hasBackBtn={true} handleBackBtnClick={handleBackBtnClick} />
+      <ProfileSection
+        // userId={userId}
+        postsCount={accountData.cnt}
+        name={userNickname}
+        setUserNickname={setUserNickname}
+        whoProfile="notUser"
+      />
+      <PostsList
+        whoProfile="user"
+        postsCount={accountData.cnt}
+        linkTo="/recipeRegistration"
+        buttonText="등록된 레시피가 없습니다."
+        postsRecipeList={accountData.recipes}
+      />
+    </>
+  );
 };
 
 export default ProfilePage;
