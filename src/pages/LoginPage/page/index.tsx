30dc18f704851daa341f8083cad14e789173a613
backdfb <>
2024-06-04 10:51:48 +0900

    Chore: Modal.tsx MainPage components 폴더로 이동 / 로그인 페이지 모달 페이지 작동 코드 삭제

--- src/pages/LoginPage/page/index.tsx
+++ src/pages/LoginPage/page/index.tsx
@@ -1,34 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import bi from "@assets/icons/bi.png";
 import GoogleLoginButton from "../components/GoogleLoginButton";
 import KakaoLoginButton from "../components/KakaoLoginButton";
-import Modal from "./Modal";
 
 const LoginPage: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleLoginClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleSubmit = (name: string, gender: string, age: string) => {
-    console.log("Submitted Info:", { name, gender, age });
-  };
-
   return (
     <div className="flex flex-col items-center pt-[100px]">
       <div className="w-[350px] h-[350px]">
         <img src={bi} alt="BrandIcon" className="mx-auto"></img>
       </div>
       <div className="flex flex-col gap-4 mt-[75px]">
-        <KakaoLoginButton onClick={handleLoginClick} />
+        <KakaoLoginButton />
         <GoogleLoginButton />
       </div>
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} onSubmit={handleSubmit} />
     </div>
   );
 };
