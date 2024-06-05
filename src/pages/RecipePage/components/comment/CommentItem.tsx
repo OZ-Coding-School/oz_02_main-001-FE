4f02ba66c8a3af11bb7f2ff25879ce1f08bad674
jjaeho0415 <jjh0773@naver.com>
2024-06-05 12:19:36 +0900

    Feat: 레시피 페이지의 댓글부분에서 프로필 사진이나 닉네임을 클릭했을때 해당 사용자의 프로필로 이동하게 하는 기능 추가

--- src/pages/RecipePage/components/comment/CommentItem.tsx
+++ src/pages/RecipePage/components/comment/CommentItem.tsx
@@ -9,19 +9,28 @@ interface CommentItemProps {
 const CommentItem: React.FC<CommentItemProps> = ({ comment }) => {
   const navigate = useNavigate();
 
-  const handleShowProfile = (userId: number): void => {};
+  const handleShowProfile = (userId: number): void => {
+    // 댓글이 사용자 본인의 댓글일때 프로필 페이지로 갈땐 id를 0으로 보내줘야함
+    let profileId = 0;
+    if (comment.canUpdate === 0) {
+      profileId = userId;
+    }
+    navigate(`/profile/${profileId}`);
+  };
 
   return (
     <div className="flex justify-between border-t p-4">
       <div className="flex gap-2">
         <img
           src={comment.profileImage}
-          className="size-[30px] rounded-full"
+          className="size-[30px] rounded-full cursor-pointer"
           onClick={() => handleShowProfile(comment.userId)}
         />
         <div>
           <div className="flex items-center gap-2">
-            <span onClick={() => handleShowProfile(comment.userId)}>{comment.userNickname}</span>
+            <span className="cursor-pointer" onClick={() => handleShowProfile(comment.userId)}>
+              {comment.userNickname}
+            </span>
             <span className="text-[#000000]/50 text-[0.8rem]">{comment.updatedAt}</span>
           </div>
           <div>{comment.comment}</div>
