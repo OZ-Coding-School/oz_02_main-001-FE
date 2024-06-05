fc99c6942e316e9a9f13c18a9a799c6e440ffb1c
backdfb <>
2024-06-05 12:43:48 +0900

    Style: 랜덤 출력 삭제, 읽음 상태에 따라 점 상태 변경

--- src/pages/NotificationPage/components/NotificationItem.tsx
+++ src/pages/NotificationPage/components/NotificationItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { NotificationType } from "src/types/notificationItemType";
 
@@ -7,41 +7,39 @@ interface NotificationItemProps {
 }
 
 const NotificationItem: React.FC<NotificationItemProps> = ({ notice }) => {
+  const [isRead, setIsRead] = useState(notice.status);
   const navigate = useNavigate();
-  const today = new Date();
-  const formattedDate = `${today.getFullYear()}.${String(today.getMonth() + 1).padStart(2, "0")}.${String(today.getDate()).padStart(2, "0")}`;
 
-  const actionTypes = [
-    { type: "like", message: notice.like },
-    { type: "reply", message: notice.reply },
-  ];
-
-  const action = actionTypes[Math.floor(Math.random() * actionTypes.length)];
-
-  const actionMessageMap = {
-    like: "레시피에 좋아요가 얼마나 눌렸는지 확인하러 가볼까요?",
-    reply: "어떤 레시피에 댓글을 남겼는지 확인하러 가볼까요?",
-  };
+  const actionMessage = notice.type === "like" ? "좋아요를 눌렀습니다." : "댓글을 남겼습니다.";
 
   const handleClick = () => {
+    setIsRead(0);
     navigate(`/recipe/${notice.recipeId}`);
   };
 
+  useEffect(() => {
+    setIsRead(notice.status);
+  }, [notice.status]);
+
   return (
     <div className="relative" onClick={handleClick} style={{ cursor: `pointer` }}>
-      {!notice.isRead && (
+      {notice.status === 1 && (
         <div className="absolute mt-2 right-[6px] h-[5px] w-[5px] bg-red-500 rounded-full"></div>
       )}
       <div className="pt-3">
         <span className="ml-2 text-sm">
-          <strong>{notice.nickname}</strong>님이 {notice.title} 레시피에 {action.message}
+          <strong>{notice.nickname}</strong>님이 {notice.title} 레시피에 {actionMessage}
         </span>
       </div>
       <div className="ml-2 mt-2">
-        <span className="text-gray-400 text-sm">{actionMessageMap[action.type]}</span>
+        <span className="text-gray-400 text-sm">
+          {notice.type === "like"
+            ? "레시피에 좋아요가 얼마나 눌렸는지 확인하러 가볼까요?"
+            : "어떤 레시피에 댓글을 남겼는지 확인하러 가볼까요?"}
+        </span>
       </div>
       <div className="ml-2 mt-3 mb-3">
-        <span className="text-gray-400 text-xs">{formattedDate}</span>
+        <span className="text-gray-400 text-xs">{notice.date}</span>
       </div>
       <div className="border"></div>
     </div>
