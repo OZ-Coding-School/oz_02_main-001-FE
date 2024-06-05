90609f24757287b6810c70ca484a019957c9c63a
backdfb <>
2024-06-05 18:21:25 +0900

    Style: 예시용 더미데이터 추가

--- src/pages/RefrigeratorPage/page/index.tsx
+++ src/pages/RefrigeratorPage/page/index.tsx
@@ -1,12 +1,25 @@
 import Footer from "@components/footer/Footer";
 import Header from "@components/header/Header";
 import React from "react";
+import { RefrigeratorType } from "src/types/refrigeratorType";
+import RefrigeratorItem from "../components/RefrigeratorItem";
 
 const RefrigeratorPage = () => {
+  const dummyData: RefrigeratorType[] = [
+    {
+      nickname: "50번째 냉장고",
+      ingredients: [
+        {
+          id: 50,
+          name: "가지",
+        },
+      ],
+    },
+  ];
   return (
     <div>
       <Header hasBell={true} />
-      <p>RefrigeratorPage</p>
+      <RefrigeratorItem refrigerator={dummyData[0]} />
       <Footer page="refrigerator" />
     </div>
   );
