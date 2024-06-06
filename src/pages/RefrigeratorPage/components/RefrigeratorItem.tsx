2cd33044163add91e11cb53348838293fa34ab35
backdfb <>
2024-06-06 19:36:07 +0900

    Style: BigButton 컴퍼는트 index로 이동

--- src/pages/RefrigeratorPage/components/RefrigeratorItem.tsx
+++ src/pages/RefrigeratorPage/components/RefrigeratorItem.tsx
@@ -1,4 +1,3 @@
-import BigButton from "@components/buttons/BigButton";
 import React from "react";
 import { PiHamburger } from "react-icons/pi";
 import { useNavigate } from "react-router";
@@ -17,24 +16,26 @@ const RefrigeratorItem: React.FC<RefrigeratorItemProps> = ({ refrigerator }) => 
 
   return (
     <div>
-      <div className="flex items-center space-x-2 mt-6">
-        <div className="bg-[#EBF6C1] rounded-xl w-8 h-8 flex justify-center items-center ml-4">
+      <div className="flex items-center space-x-2 mt-4">
+        <div className="bg-[#EBF6C1] rounded-lg w-8 h-8 flex justify-center items-center ml-6">
           <PiHamburger className="w-6 h-6" style={{ color: "#A2A38B" }} />
         </div>
         <span className="text-gray-400">{refrigerator.nickname}님의 냉장고</span>
       </div>
-      <div className="mt-4 absolute right-6">
+      <div className="mt-4 absolute right-[22px]">
         <button
           onClick={handleClick}
-          className="border border-solid border-redPink rounded-[4px] pl-4 pr-4 pt-[3px] pb-[3px]"
+          className="border border-solid border-redPink rounded-[4px] pl-[14px] pr-[14px] pt-[2px] pb-[2px]"
         >
-          <span className="text-redPink">+ 재료 추가</span>
+          <span
+            className="text-redPink w-full text-sm font-medium"
+            style={{ letterSpacing: "0.1em" }}
+          >
+            + 재료 추가
+          </span>
         </button>
       </div>
-      <div className="mt-[70px] border w-full px-2 h-[3px] bg-gray-100"></div>
-      <div className="relative pl-6 pr-6 mb-[50px] font-bold">
-        <BigButton buttonText={"레시피 찾아보기"} />
-      </div>
+      <div className="mt-[60px] border w-full px-2 h-[3px] bg-gray-100"></div>
     </div>
   );
 };
