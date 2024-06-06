8a18770ce87bf554f6e50e85e7b6e4f32cb31a58
woic-ej <woic0102@naver.com>
2024-06-06 15:37:30 +0900

    Style: FourthStep 컴포넌트 레이아웃 완성

--- src/pages/RecipeRegistrationPage/components/Label.tsx
+++ src/pages/RecipeRegistrationPage/components/Label.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 interface LabelProps {
   name: string;
+  hasStart?: boolean;
 }
 
-const Label: React.FC<LabelProps> = ({ name }) => {
+const Label: React.FC<LabelProps> = ({ name, hasStart = true }) => {
   return (
     <div className="flex gap-1">
-      <span className="font-bold">{name}</span>
-      <span className="text-redPink">*</span>
+      <span className="font-bold text-[1.1rem]">{name}</span>
+      {hasStart && <span className="text-redPink">*</span>}
     </div>
   );
 };
