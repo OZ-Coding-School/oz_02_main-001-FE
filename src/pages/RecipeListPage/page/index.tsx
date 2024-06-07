f87db4fcfcab8fa8c2eca28fdca329202b15190f
woic-ej <woic0102@naver.com>
2024-06-07 15:14:44 +0900

    Style: RecipeListPage 레이아웃 완료

--- src/pages/RecipeListPage/page/index.tsx
+++ src/pages/RecipeListPage/page/index.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import FilteringButtons from "@components/filtering/FilteringButtons";
+import Header from "@components/header/Header";
+import RecipeList from "@components/recipe/RecipeList";
+import React, { useEffect } from "react";
+import { data } from "../../SearchPage/page";
+import { useNavigate, useParams } from "react-router-dom";
 
 const RecipeListPage: React.FC = () => {
-  return <div>RecipeListPage</div>;
+  const { categoryId } = useParams();
+  const navigate = useNavigate();
+  const handleBackBtnClick = () => {
+    navigate(-1);
+  };
+
+  let title = "";
+  if (categoryId === "daily") {
+    title = "일상요리";
+  } else if (categoryId === "healthy") {
+    title = "건강요리";
+  } else if (categoryId === "midnightSnack") {
+    title = "야식";
+  } else if (categoryId === "desert") {
+    title = "디저트";
+  }
+
+  return (
+    <div>
+      <Header hasBackBtn={true} title={title} handleBackBtnClick={handleBackBtnClick} />
+      <FilteringButtons />
+      <RecipeList recipeData={data} />
+    </div>
+  );
 };
 
 export default RecipeListPage;
