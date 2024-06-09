50d993a44cf88a3c4076aadffd963c5d1aabd991
backdfb <>
2024-06-09 20:16:54 +0900

    Style: 간격 최종 수정

--- src/pages/IngredientsListPage/components/IngredientsListItem.tsx
+++ src/pages/IngredientsListPage/components/IngredientsListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IngredientDataType } from "src/types/ingredientType";
 import RoundedSmallButton from "@components/buttons/RoundedSmallButton";
 
@@ -10,16 +10,25 @@ interface IngredientsListItemProps {
 
 const IngredientsListItem: React.FC<IngredientsListItemProps> = ({
   ingredient,
-  buttonState,
   toggleButtonState,
 }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    if (ingredient.id !== undefined) {
+      toggleButtonState(ingredient.id);
+      setIsClicked(!isClicked);
+    }
+  };
+
   return (
     <div>
-      <div className="flex justify-between items-center my-2 py-2">
-        <span className="">{ingredient.name}</span>
+      <div className="flex justify-between items-center pl-10 pr-5 py-5">
+        <span>{ingredient.name}</span>
         <RoundedSmallButton
-          handleClick={() => toggleButtonState(ingredient.id)}
-          buttonText={buttonState ? "취소" : "추가"}
+          handleClick={handleClick}
+          buttonText={isClicked ? "취소" : "추가"}
+          isClicked={isClicked}
         />
       </div>
       <div className="border border-solid"></div>
