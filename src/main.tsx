ca229de7fab7e75b0a26fe5cc56f83120b4e2ed3
jjaeho0415 <jjh0773@naver.com>
2024-06-10 11:57:17 +0900

    Conf: reactQueryDevTools 설치 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // QueryClient 인스턴스를 생성
 const queryClient = new QueryClient();
@@ -9,5 +10,6 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={true} />
   </QueryClientProvider>,
 );
