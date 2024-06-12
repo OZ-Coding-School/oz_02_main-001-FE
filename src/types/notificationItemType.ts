bb25d7053562c3a828aac0c18ab0a4e3346a76ff
backdfb <>
2024-06-12 13:02:53 +0900

    Chore: data type 수정

--- src/types/notificationItemType.ts
+++ src/types/notificationItemType.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 export type NotificationType = {
-  recipeId: number; //백엔드에 요청함
-=======
-type NotificationType = {
   recipeId?: number; //백엔드에 요청함
->>>>>>> dev
   nickname: string;
   title: string;
   type: string;
