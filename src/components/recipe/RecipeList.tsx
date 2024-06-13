2fbb06302e9d305b341eac39356427dcf298b604
backdfb <>
2024-06-13 11:43:52 +0900

    Feat: 레시피 목록 페이지 더보기 기능 구현

--- src/components/recipe/RecipeList.tsx
+++ src/components/recipe/RecipeList.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import RecipeItem from "@components/recipe/RecipeItem";
+import BigButton from "@components/buttons/BigButton";
 
 interface RecipeListProps {
   recipeData: RecipeType[];
 }
 
 const RecipeList: React.FC<RecipeListProps> = ({ recipeData }) => {
+  const [more, setMore] = useState(3);
+  const handleLoadMore = () => {
+    setMore((prevCount) => prevCount + 3);
+  };
+  {
+    /* more버튼 클릭시 3개씩 출력 */
+  }
+
   return (
     <div className="flex flex-col gap-3 p-3">
-      {recipeData.map((recipe) => (
+      {recipeData.slice(0, more).map((recipe) => (
         <RecipeItem key={recipe.id} recipe={recipe} />
       ))}
+      {more < recipeData.length && <BigButton buttonText={"더보기"} handleClick={handleLoadMore} />}
     </div>
   );
 };
