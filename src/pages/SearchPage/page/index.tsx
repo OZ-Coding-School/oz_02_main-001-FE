d904e3b4dcba259ed0fb52bcc4175cab966f53e0
woic-ej <woic0102@naver.com>
2024-06-15 19:46:56 +0900

    Chore: 필요없는 이미지 파일 삭제

--- src/pages/SearchPage/page/index.tsx
+++ src/pages/SearchPage/page/index.tsx
@@ -2,108 +2,13 @@ import FilteringButtons from "@components/filtering/FilteringButtons";
 import SearchHeader from "@components/header/SearchHeader";
 import RecipeList from "@components/recipe/RecipeList";
 import React from "react";
-import recipeImg from "@assets/images/recipe.png";
-
-export const data: RecipeType[] = [
-  {
-    id: 1,
-    user: "1번째 냉장고",
-    title: "오징어 볶음",
-    image: recipeImg,
-    like: 130,
-    likeStatus: 1,
-    book: 50,
-    bookStatus: 1,
-  },
-  {
-    id: 2,
-    user: "2번째 냉장고",
-    title: "토마토 볶음",
-    image: recipeImg,
-    like: 30,
-    likeStatus: 1,
-    book: 0,
-    bookStatus: -1,
-  },
-  {
-    id: 3,
-    user: "3번째 냉장고",
-    title: "제육볶음",
-    image: recipeImg,
-    like: 10,
-    likeStatus: -1,
-    book: 10,
-    bookStatus: 1,
-  },
-  {
-    id: 3,
-    user: "3번째 냉장고",
-    title: "제육볶음",
-    image: recipeImg,
-    like: 10,
-    likeStatus: -1,
-    book: 10,
-    bookStatus: 1,
-  },
-
-  {
-    id: 3,
-    user: "3번째 냉장고",
-    title: "제육볶음",
-    image: recipeImg,
-    like: 10,
-    likeStatus: -1,
-    book: 10,
-    bookStatus: 1,
-  },
-  {
-    id: 3,
-    user: "3번째 냉장고",
-    title: "제육볶음",
-    image: recipeImg,
-    like: 10,
-    likeStatus: -1,
-    book: 10,
-    bookStatus: 1,
-  },
-  {
-    id: 3,
-    user: "3번째 냉장고",
-    title: "제육볶음",
-    image: recipeImg,
-    like: 10,
-    likeStatus: -1,
-    book: 10,
-    bookStatus: 1,
-  },
-  {
-    id: 3,
-    user: "3번째 냉장고",
-    title: "제육볶음",
-    image: recipeImg,
-    like: 10,
-    likeStatus: -1,
-    book: 10,
-    bookStatus: 1,
-  },
-  {
-    id: 3,
-    user: "3번째 냉장고",
-    title: "제육볶음",
-    image: recipeImg,
-    like: 10,
-    likeStatus: -1,
-    book: 10,
-    bookStatus: 1,
-  },
-];
 
 const SearchPage: React.FC = () => {
   return (
     <div>
       <SearchHeader />
       <FilteringButtons />
-      <RecipeList recipeData={data} />
+      <RecipeList />
     </div>
   );
 };
