25ede1a397a40a09b692e3fcfb69f465705ea220
jjaeho0415 <jjh0773@naver.com>
2024-06-17 19:33:08 +0900

    Fix: 수정

--- src/routes/CommonRoute.tsx
+++ src/routes/CommonRoute.tsx
@@ -5,11 +5,11 @@ import { apiRoutes } from "../api/apiRoutes";
 import { fetchData } from "../api/axios";
 
 const CommonRoute: React.FC = () => {
-  const fetchLogin = async (): Promise<void> => {
+  const fetchLogin = async (): Promise<FetchAlertsStatusType> => {
     return await fetchData("GET", apiRoutes.userLogin);
   };
 
-  const { data, isFetched } = useQuery<void, void>({
+  const { data, isFetched } = useQuery<FetchAlertsStatusType, void>({
     queryFn: fetchLogin,
     queryKey: ["login"],
   });
@@ -17,7 +17,7 @@ const CommonRoute: React.FC = () => {
     return null;
   }
   // 로그인 상태면 무조건 홈으로 리디렉션
-  return data ? <Navigate to={"/"} /> : <Outlet />;
+  return data?.status === 200 ? <Navigate to={"/"} /> : <Outlet />;
 };
 
 export default CommonRoute;
